c4f22102a9608e61fe44c2a66204dc060d2afd8a
José Miguel 1507 <git@github.com:Jose-Miguel-1507/jetsmart-clone.git>
2025-04-22 19:21:48 -0500

    ajuste numero 2, pag compra o pago

---
+++ assets/js/compras.js
@@ -0,0 +1,21 @@
+let cantidad = 1;
+
+function cambiarCantidad(valor) {
+  cantidad += valor;
+  if (cantidad < 1) cantidad = 1;
+  document.getElementById("cantidad").innerText = cantidad;
+  actualizarPrecio();
+}
+
+function actualizarPrecio() {
+  const precioUnitario = 40000;
+  const envio = 1000;
+  const iva = Math.round((precioUnitario * cantidad) * 0.13);
+  const total = precioUnitario * cantidad + envio + iva;
+
+  document.getElementById("precio-producto").innerText = `$${precioUnitario * cantidad}`;
+  document.getElementById("total").innerText = `$${total}`;
+}
+
+// Ejecutar al inicio
+actualizarPrecio();
